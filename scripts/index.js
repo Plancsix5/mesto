3c579944405af0514367da5b4b3187593aca8191
Moskalenko Platon <mosckalencko.plat@yandex.ru>
2023-05-13 17:00:13 +0300

    v4.5

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,22 @@ let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileWorkplace = document.querySelector('.profile__workplace');
-let popupContent = document.forms.popup__content;
+let popupName = document.querySelector('.popup__profile-form-input_type_name');
+let popupWorkplace = document.querySelector('.popup__profile-form-input_type_workplace');
+let formProfile = document.querySelector('.popup__content');
 
 openButton.addEventListener('click', function(){
     popup.classList.add('popup_opened')
-    popupContent.elements.name.value = profileName.textContent 
-    popupContent.elements.workplace.value = profileWorkplace.textContent 
+    popupName.value = profileName.textContent 
+    popupWorkplace.value = profileWorkplace.textContent 
 });
 closeButton.addEventListener('click', function(){
     popup.classList.remove('popup_opened')
 });
 
-popupContent.addEventListener('submit', function(event){
+formProfile.addEventListener('submit', function(event){
     event.preventDefault();
-    profileName.textContent = popupContent.elements.name.value
-    profileWorkplace.textContent = popupContent.elements.workplace.value
+    profileName.textContent = popupName.value
+    profileWorkplace.textContent = popupWorkplace.value
     popup.classList.remove('popup_opened')
 })
